13c882f6560506bac9b33bdafa63ce1d6ab7343f
AlessandroPalumboDev <alessandropalumbo.dev@gmail.com>
2024-04-24 21:16:26 +0200

    [UPD] Obbligo che i dati inseriti siano realmente numeri

--- js/main.js
+++ js/main.js
@@ -5,3 +5,23 @@ const km = Number(prompt("Dichiarare numero di Km da percorrere"));
 
 // Creo un prompt che chieda l'età del passeggero creando una variabile trasformandola in numero
 const eta = Number(prompt("Dichiarare età in anni"));
+
+// Se i dati inseriti dall'utente sono realmente numeri
+if (!isNaN(km) && !isNaN(eta)) {
+
+
+
+
+
+
+ 
+    
+
+
+
+    
+}    
+    // Altrimenti esce questo avviso cattivissimo
+else {
+    console.log("PUOI INSERIRE SOLO NUMERI!!");
+}
