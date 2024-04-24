87df9206edbf607b7877a4f980ea5e7bd5b8d02e
AlessandroPalumboDev <alessandropalumbo.dev@gmail.com>
2024-04-24 21:14:01 +0200

    [UPD] Creazione prompt

--- js/main.js
+++ js/main.js
@@ -1,1 +1,7 @@
 "use strict"
+
+// Creo un prompt che chieda i chilometri da percorrere creando una variabile trasformandola in numero
+const km = Number(prompt("Dichiarare numero di Km da percorrere"));
+
+// Creo un prompt che chieda l'età del passeggero creando una variabile trasformandola in numero
+const eta = Number(prompt("Dichiarare età in anni"));
