62543163f45170920e7afd01179152fbb9cd03a6
AlessandroPalumboDev <alessandropalumbo.dev@gmail.com>
2024-04-25 22:28:18 +0200

    [FIX] Trasforma dato utente in minuscolo

--- bonus/js/bonus.js
+++ bonus/js/bonus.js
@@ -1,7 +1,7 @@
 "use strict"
 
 // definisco una variabile con il valore di un prompt che chiede pari o dispari
-const sceltaUtente = prompt('pari o dispari?');
+const sceltaUtente = prompt('pari o dispari?').toLowerCase();
 
 // definisco una variabile con un valore di prompt 1-9
 const numeroUtente = Number(prompt('Scegli un numero da 1 a 9'));
